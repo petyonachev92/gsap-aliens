bae367ab2011e9d73fe865dda000eab2e3330af4
Petyo <pyo@abv.bg>
2021-03-02 10:41:01 +0200

    Fixed event BEAM_SHOW

--- src/app/custom/Saucer.js
+++ src/app/custom/Saucer.js
@@ -4,7 +4,7 @@ import gsap from "gsap/all"
 const EVENTS = {
     FLY_IN: 'fly_in',
     FLY_AWAY: 'fly_away',
-    BEAM_SHOW: 'beam_show',
+    BEAM_SHOW: 'beam_showed',
     BEAM_HIDE: 'beam_hide'
 }
 
